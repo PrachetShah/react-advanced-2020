70faa266258114b1fde5ef7bd3688325f75340b1
PrachetShah <prachetshah25@gmail.com>
2022-03-02 18:21:05 +0530

    conditional rendering eg3 show-hide using useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
+import Setup from "./tutorial/3-conditional-rendering/setup/3-show-hide";
 // import Final from "./tutorial/2-useEffect/final/3-useEffect-fetch-data";
 
 function App() {
