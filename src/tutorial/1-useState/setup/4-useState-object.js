ed47ddc2aaf56dd41c68e91a9aeaea3a571cf781
PrachetShah <prachetshah25@gmail.com>
2022-03-02 09:13:58 +0530

    completed useEffect example 4 and 5

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Peter",
+    age: 24,
+    msg: "Help Me Peter Parker",
+  });
+
+  const changeMessage = () => {
+    // Here we use spread operator to copy the rest of values and only change message in this case so whole object is not deleted
+    setPerson({ ...person, msg: "hello world" });
+  };
+
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>Message: {person.msg}</h3>
+      <button className="btn" onClick={changeMessage}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+  const reset = () => {
+    setValue(0);
+  };
+  const complexIncrease = () => {
+    //Needs 2 things, callback func and timer ie 2 sec here
+    setTimeout(() => {
+      //setValue(value + 1);
+      /*it works in this case as we are getting correct old value, 
+      in prevcase we used to get 0 before time and therefore it did not update
+      */
+      setValue((prevValue) => {
+        return prevValue + 1;
+      });
+    }, 2000);
+  };
+
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Regular Counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          Decrease
+        </button>
+        <button className="btn" onClick={reset}>
+          Reset
+        </button>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          Increase
+        </button>
+      </section>
+
+      <section style={{ margin: "4rem 0" }}>
+        <h2>More Complex Counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}>
+          Increase later
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
