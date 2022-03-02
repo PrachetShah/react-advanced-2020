522795266590b14d9a42954a0c6629fe7945323c
PrachetShah <prachetshah25@gmail.com>
2022-03-02 19:40:48 +0530

    multiple inputs forms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/4-forms/setup/1-controlled-inputs";
+import Setup from "./tutorial/4-forms/setup/2-multiple-inputs";
 // import Final from "./tutorial/2-useEffect/final/3-useEffect-fetch-data";
 
 function App() {
