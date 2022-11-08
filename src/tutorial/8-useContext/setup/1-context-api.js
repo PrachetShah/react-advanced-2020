e97017f3002e99dd924710cb680af97764552c50
PrachetShah <prachetshah25@gmail.com>
2022-11-08 16:54:11 +0530

    useContext Hook Information + Code

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -1,7 +1,11 @@
-import React, { useState, useContext } from 'react';
-import { data } from '../../../data';
+import React, { useState, useContext } from "react";
+import { data } from "../../../data";
 // more components
 // fix - context api, redux (for more complex cases)
+
+const PersonContext = React.createContext();
+// Once we create Context, we have access to 2 components, the provider and the consumer
+// We dont use Consumer now as React has useContext hook now, previosuly we needed it
 
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
@@ -10,33 +14,34 @@ const ContextAPI = () => {
       return people.filter((person) => person.id !== id);
     });
   };
+
+  // PersonContext.Provider is needed on root of the tree to use it later
   return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+    <PersonContext.Provider value={{ removePerson, people }}>
+      <h3>ContextAPI / useContext Hook</h3>
+      <List />
+    </PersonContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = () => {
+  const mainData = useContext(PersonContext);
+  console.log(mainData);
   return (
     <>
-      {people.map((person) => {
-        return (
-          <SinglePerson
-            key={person.id}
-            {...person}
-            removePerson={removePerson}
-          />
-        );
+      {mainData.people.map((person) => {
+        return <SinglePerson key={person.id} {...person} />;
       })}
     </>
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  // We can directly destructure it from object {people: PEOPLE, removePerson: function}
+  const { removePerson } = useContext(PersonContext);
+  console.log(data);
   return (
-    <div className='item'>
+    <div className="item">
       <h4>{name}</h4>
       <button onClick={() => removePerson(id)}>remove</button>
     </div>
